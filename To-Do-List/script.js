02b80dfacd54784e1b2d084ae2b6e18257cb1226
Dabhi Amit <amitdabhi526@gmail.com>
2025-08-02 13:21:45 +0530

    to-do-list

---
+++ To-Do-List/script.js
@@ -0,0 +1,30 @@
+function mewElement(){
+    let li = document.createElement("li")
+    let inputValue = document.getElementById("getInput").value;
+    let a = document.createTextNode(inputValue)
+    li.appendChild(a)
+    if(inputValue == " "){
+        alert("You must write something!")
+    }
+    else{
+        document.getElementById("displayList").appendChild(li)
+    }
+
+    document.getElementById("getInput").value;
+
+    let span = document.createElement("SPAN")
+    let txt = document.createTextNode("\u00D7");
+
+    span.className = "close"
+    span.appendChild(txt)
+    li.appendChild(span)
+
+    for(i=0; i<close.length; i++){
+        close[i].onclick = function(){
+            let div = this.parentElement
+            div.style.display = "none";
+        }
+    }
+
+    input.value = ""
+}
